82d27c82cc630c75cadca1d996f0b665ea520bf6
DenysPetroff <petroffich2008@gmail.com>
2023-08-04 10:38:09 +0300

    third

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { getOneCategoriesBooksFromApi } from './fetch_API_categories.js';
 // let category = 'Hardcover Nonfiction';
-const categoryBookContainerGellary = document.querySelector('.gallery-category-books-container-js');
+const categoryBookContainerGellary = document.querySelector('.top-category-list');
 
 let categories = 'Childrens Middle Grade Hardcover';
 getOneCategoriesBooksFromApi(categories).then(resp => {
@@ -11,15 +11,20 @@ getOneCategoriesBooksFromApi(categories).then(resp => {
 
  function createGalleryCategoriesBooksMarkup(data) { 
      
-   const cartBook = data.map(({book_image, title, author }) => `
-<div class="photo-card">
-      <img src="${book_image}" alt="${title}" loading="lazy" />
-      <div class="info">
-        <p class="title-book">${title}</p>
-        <p class="author-name">${author}</p>      
-      </div>
-</div>
-    `).join('');
+   const cartBook = data.map(({_id, book_image, title, author }) => 
+//    `<div class="photo-card">
+//       <img src="${book_image}" alt="${title}" loading="lazy" />
+//       <div class="info">
+//         <p class="title-book">${title}</p>
+//         <p class="author-name">${author}</p>      
+//       </div>
+// </div>`
+   `<div class="book-card" data-id="${_id}">
+         <img src="${book_image}" alt="${title}" class="book-card-img" width="300" height="300">
+       <h4 class="book-card-title">${title}</h4>
+        <p class="book-card-author">${author}</p>
+       </div>`
+   ).join('');
     categoryBookContainerGellary.innerHTML = '';
     categoryBookContainerGellary.insertAdjacentHTML('beforeend', cartBook);
 };
